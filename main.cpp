0d77ddfdc9f0c83e5284bb14986970f4dd905b6c
OtVeRtKa-boop <stpnstpnvc@gmail.com>
2020-01-09 21:27:50 +0400

    made array global

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@
 using namespace std;
 
 constexpr int n = 6;
+array<int,n> xi{-1,0,1,2,3,4};
+array<int,n> yi{-243,-32,-1,0,1,32};
 
-double lagrange(array<int,n> x, array<int,n> y, double _x);
+double lagrange(double _x);
 
 int main() {
-    array<int,n> xi{-1,0,1,2,3,4};
-    array<int,n> yi{-243,-32,-1,0,1,32};
 
     for(const auto i : xi)
-        cout << lagrange(xi,yi,i)<<" ";
+        cout << lagrange(i)<<" ";
     cout << endl;
     string L;
     for (int j = 0; j < n; ++j) {
-        L+= to_string(yi[j]*lagrange(xi,yi,j)) + '\t';
+        L+= to_string(yi[j]*lagrange(j)/lagrange(xi[j])) + '\t';
     }
     cout << L;
     return 0;
 }
-double lagrange(array<int,n> x, array<int,n> y, double _x)
+double lagrange(double _x)
 {
     double result = 0.0;
     for (short i = 0; i < n; i++)
@@ -29,8 +29,8 @@ double lagrange(array<int,n> x, array<int,n> y, double _x)
         double P = 1.0;
         for (short j = 0; j < n; j++)
             if (j != i)
-                P *= (_x - x[j])/ (x[i] - x[j]);
-        result += P * y[i];
+                P *= (_x - xi[j])/(xi[i] - xi[j]);
+        result += P * yi[i];
     }
     return result;
 }
